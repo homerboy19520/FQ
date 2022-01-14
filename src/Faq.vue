9c0c31a325f1229a10bf8bd83ee4dfe213868707
Артур Филимонцев <homerboy19520@mail.ru>
2022-01-14 12:21:48 +0300

    push project

---
+++ src/Faq.vue
@@ -0,0 +1,290 @@
+<template>
+  <div class="faq">
+    <div class="faq__container">
+      <img
+        class="faq__img faq__img_mobile"
+        src="/backgroundWoman-mobile.png"
+        alt="woman"
+      />
+      <div class="faq__box">
+        <div class="faq__background">
+          <img
+            class="faq__img faq__img_woman"
+            src="/backgroundWoman.png"
+            alt="woman"
+          />
+          <img
+            class="faq__img faq__img_box"
+            src="/backgrowndBox.png"
+            alt="figure"
+          />
+          <img
+            class="faq__img faq__img_line"
+            src="/backgrowndLine.png"
+            alt="figure"
+          />
+          <img
+            class="faq__img faq__img_square"
+            src="/backgrowndSquare.png"
+            alt="figure"
+          />
+        </div>
+      </div>
+      <div class="main">
+        <section class="section">
+          <h1 class="section__title">FAQ</h1>
+          <div class="section__container">
+            <dropdown
+              v-for="(item, index) in mokData"
+              :key="item.id"
+              :id="item.id"
+              :question="item.question"
+              :answer="item.answer"
+              :isOpen="state[index]"
+              @click="onClick"
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import dropdown from "./components/dropdown.vue";
+
+export default {
+  name: "Faq",
+  components: { dropdown },
+
+  data() {
+    return {
+      initialState: [],
+      state: [],
+      mokData: [
+        {
+          id: 0,
+          question: "How many team members can I invite?",
+          answer: "You can invite many people",
+        },
+        {
+          id: 1,
+          question: "What is the maximum file upload size?",
+          answer:
+            "No more than 2GB. All files in your account must fit your allotted storage space.",
+        },
+        {
+          id: 2,
+          question: "How do I reset my password?",
+          answer:
+            "Log into your personal account and click the button with the text reset",
+        },
+        {
+          id: 3,
+          question: "Can I cancel my subscription?",
+          answer: "Of course, you must follow the links and unsubscribe",
+        },
+        {
+          id: 4,
+          question: "Do you provide additional support?",
+          answer:
+            "Аt the end of the landing page there is a form with support requests",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    onClick(index) {
+      let elementState = this.state[index];
+      this.closeAll();
+      if (elementState == true) {
+        this.state[index] == false;
+      } else {
+        this.state[index] = true;
+      }
+    },
+    closeAll() {
+      this.state = [...this.initialState];
+    },
+  },
+
+  mounted() {
+    this.mokData.forEach(() => this.initialState.push(false));
+    this.closeAll();
+  },
+};
+</script>
+
+<style lang="scss">
+.faq {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+
+  &__container {
+    position: relative;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-start;
+    padding: 65px 95px 30px 0;
+    width: 100%;
+    height: 100%;
+    max-width: 920px;
+    min-height: 510px;
+    box-sizing: border-box;
+    border-radius: 23px;
+    box-shadow: 0px 50px 50px -20px rgba(53, 18, 122, 0.497159);
+    background-color: #fff;
+
+    @media (max-width: 980px) {
+      padding: 24px 67px 0 0;
+      min-height: 380px;
+    }
+
+    @media (max-width: 910px) {
+      justify-content: center;
+      align-items: flex-start;
+      padding: 20px 0 0 0;
+    }
+
+    @media (max-width: 600px) {
+      padding: 138px 24px 0 24px;
+    }
+  }
+
+  &__img {
+    position: absolute;
+    top: 70px;
+    left: 0;
+
+    @media (max-width: 980px) {
+      top: 20px;
+    }
+
+    @media (max-width: 910px) {
+      filter: blur(4px);
+    }
+
+    @media (max-width: 600px) {
+      display: none;
+    }
+
+    &_line {
+      top: 0;
+      z-index: 0;
+
+      @media (max-width: 910px) {
+        display: none;
+      }
+    }
+
+    &_square {
+      bottom: 0;
+      z-index: 1;
+      @media (max-width: 910px) {
+        display: none;
+      }
+    }
+
+    &_box {
+      top: 206px;
+      z-index: 3;
+      transform: translate(-50%, 0);
+    }
+
+    &_woman {
+      z-index: 2;
+      transform: translate(-84px, 0);
+      height: 358px;
+      width: auto;
+
+      @media (max-width: 980px) {
+        height: 320px;
+      }
+    }
+
+    &_mobile {
+      display: none;
+      filter: blur(0);
+      height: 240px;
+      left: 50%;
+      top: 0;
+      transform: translate(-50%, -50%);
+      @media (max-width: 600px) {
+        display: block;
+      }
+    }
+  }
+
+  &__box {
+    position: absolute;
+    left: 0;
+    top: 0;
+
+    @media (max-width: 910px) {
+      left: calc(50% - 124px);
+    }
+  }
+
+  &__background {
+    position: relative;
+  }
+}
+
+.main {
+  min-width: 350px;
+  max-width: 350px;
+  z-index: 3;
+
+  @media (max-width: 600px) {
+    max-width: none;
+    min-width: 0;
+    width: 100%;
+    min-height: 346px;
+  }
+
+  @media (max-width: 500px) {
+    max-width: none;
+    min-width: 0;
+    width: 100%;
+    min-height: 400px;
+  }
+}
+
+.section {
+  &__title {
+    font-family: "Kumbh Sans", sans-serif;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 40px;
+    color: #1e1f36;
+    margin: 0 0 35px 0;
+    user-select: none;
+
+    @media (max-width: 600px) {
+      text-align: center;
+    }
+  }
+
+  &__container {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+}
+
+body {
+  margin: 0;
+  font-family: "Kumbh Sans", sans-serif;
+  background: linear-gradient(180deg, #b068e9 0%, #6463e7 100%);
+  padding: 0 70px;
+  min-width: 320px;
+
+  @media (max-width: 600px) {
+    padding: 0 24px;
+  }
+}
+</style>
---
+++ src/components/dropdown.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="dropdown" :class="{ 'm-open': this.isOpen }">
+    <div class="dropdown__header" @click="$emit('click', id)">
+      <p class="dropdown__question">{{ question }}</p>
+      <svg
+        class="dropdown__icon"
+        width="10"
+        height="7"
+        viewBox="0 0 10 7"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 0.798645L5 4.79865L9 0.798645"
+          stroke="#F47B56"
+          stroke-width="2"
+        />
+      </svg>
+    </div>
+    <p class="dropdown__answer" v-show="isOpen">{{ answer }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      require: true,
+    },
+    isOpen: {
+      type: Boolean,
+      require: true,
+    },
+    question: {
+      type: String,
+      require: true,
+    },
+    answer: {
+      type: String,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.dropdown {
+  position: relative;
+  border-bottom: 1px solid #e8e8ea;
+  padding-bottom: 16px;
+  overflow: hidden;
+  height: auto;
+
+  &.m-open {
+    .dropdown__icon {
+      transform: rotate(180deg);
+    }
+
+    .dropdown__question {
+      font-weight: 700;
+      color: #1e1f36;
+    }
+  }
+
+  &__icon {
+    position: absolute;
+    top: 7px;
+    right: 16px;
+    transition: transform 0.2s ease-out;
+    cursor: pointer;
+  }
+
+  &__question {
+    font-family: "Kumbh Sans", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    color: #4b4c5f;
+    margin: 0;
+    transition: color 0.2s ease-out;
+    cursor: pointer;
+
+    &:hover {
+      color: #f47b56;
+    }
+
+    @media (max-width: 600px) {
+      font-size: 13px;
+      line-height: 16px;
+    }
+  }
+
+  &__answer {
+    font-family: "Kumbh Sans", sans-serif;
+    padding-top: 11px;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+    color: #787887;
+  }
+
+  & p,
+  & img {
+    margin: 0;
+    user-select: none;
+  }
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+.answerAnimation-enter-active {
+  transition: all 0.2s ease;
+}
+.answerAnimation-leave-active {
+  transition: all 0.2s ease;
+}
+.answerAnimation-enter,
+.answerAnimation-leave-to {
+  transform: translateX(30px);
+  opacity: 0;
+}
+</style>
